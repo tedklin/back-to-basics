a412718c89870e6770ad418448a036942ed60107
tedklin <tedklin@gmail.com>
2020-07-13 12:34:22 -0700

    finish graph class impl

--- algorithms/graphs/graph.hpp
+++ algorithms/graphs/graph.hpp
@@ -1,12 +1,8 @@
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-
 /**
- * This is my attempt at defining a graph class type from scratch, based on
- * concepts in Skiena but using different underlying data structures. The
- * primary goal is to be as intuitive as possible, possibly at the expense of
- * space overhead.
+ * This is my attempt at defining a graph class type completely from scratch,
+ * based on concepts in Skiena but using different underlying data structures.
+ * The primary goal is to be as intuitive as possible, with the potential
+ * expense of space overhead.
  *
  * The concept of weights is supported but optional. Unweighted graphs simply
  * use implicit default values of 1.
@@ -50,9 +46,9 @@
   weights.
 
   Thus, the adjacency list in the example above could be thought of abstractly
-  as the following literal:
+  as the following literal representation:
 
-    {
+    rep1 = {
         {A, {D, E}},
         {B, {}},
         {C, {E}},
@@ -64,7 +60,7 @@
   implicit weights of 1 for an unweighted graph) would have a literal
   representation of:
 
-    {
+    rep2 = {
         {A, {{D, 1}, {E, 1}}},
         {B, {}},
         {C, {{E, 1}}},
@@ -73,25 +69,54 @@
     }
 
   Both can be passed in as a literal initializers for the "Graph" class, with
-  the first implicitly resulting in an unweighted graph with the second
-  underlying structure.
+  "rep1" implicitly resulting in an unweighted graph with the underlying
+  structure "rep2".
+
+  Furthermore, since this is an undirected graph (i.e. existence of edge {A, D}
+  implies edge {D, A}), the "Graph" class also has support for passing in
+  literal intializers that don't include duplicate edges.
+
+    rep3 = {
+        {A, {D, E}},
+        {B, {}},
+        {C, {}},
+        {D, {E}},
+        {E, {C}}
+    }
+
+  "rep3" would also result in the underlying structure "rep2".
+
+  See the "example" function in test.cpp for verification that this works.
 
  */
 
-class Vertex {
- public:
+#include <iostream>  // TODO: remove
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+struct Vertex {
   enum State { UNDISCOVERED, DISCOVERED, PROCESSED };
 
   Vertex(const std::string& name, double weight = 1)
       : name_(name), weight_(weight) {}
 
-  bool operator==(const Vertex& rhs) { return (name_ == rhs.name_); }
-  bool operator!=(const Vertex& rhs) { return !operator==(rhs); }
-
   std::string name_;
   double weight_;
   State state_ = State::UNDISCOVERED;
 };
+
+inline bool operator<(const Vertex& lhs, const Vertex& rhs) {
+  return (lhs.name_ < rhs.name_);
+}
+
+inline bool operator==(const Vertex& lhs, const Vertex& rhs) {
+  return (lhs.name_ == rhs.name_);
+}
+
+inline bool operator!=(const Vertex& lhs, const Vertex& rhs) {
+  return !operator==(lhs, rhs);
+}
 
 // Overload std::hash for our self-defined Vertex type.
 // https://en.cppreference.com/w/cpp/container/unordered_map/unordered_map
@@ -104,29 +129,76 @@ struct hash<Vertex> {
 };
 }  // namespace std
 
+// TODO: bfs, dfs
 class Graph {
  public:
   using EdgeSet = std::unordered_map<Vertex, double>;
   using AdjacencyList = std::unordered_map<Vertex, EdgeSet>;
 
+  // Convenience typename used for user input; not actual underlying type.
+  using InputUnweightedAL =
+      std::unordered_map<Vertex, std::unordered_set<Vertex>>;
+
   Graph(bool is_directed) : is_directed_(is_directed) {}
 
   // Creates a graph given only a set of vertices, no edges.
-  Graph(std::unordered_set<Vertex> vertex_set, bool is_directed)
-      : is_directed_(is_directed) {}
+  Graph(const std::unordered_set<Vertex>& vertex_set, bool is_directed)
+      : is_directed_(is_directed) {
+    for (const Vertex& v : vertex_set) {
+      add_vertex(v);
+    }
+  }
 
   // Creates a graph without specified edge weights.
-  Graph(std::unordered_map<Vertex, std::unordered_set<Vertex>> unweighted_al,
-        bool is_directed)
-      : is_directed_(is_directed) {}
+  Graph(InputUnweightedAL unweighted_al, bool is_directed)
+      : is_directed_(is_directed) {
+    for (const auto& p : unweighted_al) {
+      Vertex source = p.first;
+      add_vertex(source);
+      for (const Vertex& dest : p.second) {
+        add_edge(source, dest);
+      }
+    }
+  }
 
   Graph(AdjacencyList adjacency_list, bool is_directed)
       : adjacency_list_(adjacency_list), is_directed_(is_directed) {}
 
-  void add_edge(const Vertex& start, const Vertex& end,
-                double edge_weight = 1) {}
+  void add_edge(const Vertex& source, const Vertex& dest,
+                double edge_weight = 1) {
+    add_vertex(source);
+    add_vertex(dest);
 
-  void add_vertex(const Vertex& v, double vertex_weight = 1) {}
+    adjacency_list_[source][dest] = edge_weight;
+    if (!is_directed_) {
+      adjacency_list_[dest][source] = edge_weight;
+    }
+  }
+
+  void add_vertex(const Vertex& v) { adjacency_list_[v]; }
+
+  std::string vertex_set_str() const {
+    std::string s("Vertex set:\n");
+    for (const auto& p : adjacency_list_) {
+      s += p.first.name_ + " | ";
+    }
+    s += '\n';
+    return s;
+  }
+
+  std::string adjacency_list_str() const {
+    std::string s("Adjacency list:\n");
+    for (const auto& p : adjacency_list_) {
+      s += p.first.name_ + " -> ";
+      for (const auto& e : p.second) {
+        // recall e.second is weight
+        s += e.first.name_ + "(" + std::to_string(e.second) + ") | ";
+      }
+      s += '\n';
+    }
+    s += '\n';
+    return s;
+  }
 
  private:
   AdjacencyList adjacency_list_;
---
+++ algorithms/graphs/test.cpp
@@ -0,0 +1,93 @@
+// Quick ad hoc tests for core functionality
+
+#include "graph.hpp"
+
+#include <iostream>
+
+void equality_op_overload() {
+  Vertex v1("A");
+  Vertex v2("B");
+  Vertex v1_again("A");
+
+  std::cout << (v1 == v2) << '\n';
+  std::cout << (v1 == v1_again) << "\n\n";
+}
+
+void hash_overload() {
+  Vertex v1("A");
+  Vertex v2("B");
+  Vertex v1_again("A");
+
+  std::cout << std::hash<Vertex>{}(v1) << '\n';
+  std::cout << std::hash<Vertex>{}(v2) << '\n';
+  std::cout << std::hash<Vertex>{}(v1_again) << "\n\n";
+}
+
+void vertex_set_constructor() {
+  Vertex v1("A"), v2("B"), v3("C");
+  std::unordered_set<Vertex> vertex_set({v1, v2, v3});
+
+  // TODO: why doesn't this work? isn't there an implicit conversion from string
+  // to Vertex, as defined by the Vertex constructor?
+  // std::unordered_set<Vertex> vertex_set_conv({"A", "B", "C"});
+
+  Graph graph(vertex_set, false);
+  std::cout << graph.vertex_set_str() << "\n\n";
+}
+
+void unweighted_al_constructor() {
+  Vertex v1("A"), v2("B"), v3("C");
+
+  // These two graphs should be the same when the graph is undirected, but
+  // different when the graph is directed.
+  Graph::InputUnweightedAL unweighted_al_full(
+      {{v1, {v2, v3}}, {v2, {v1, v3}}, {v3, {v1, v2}}});
+  Graph::InputUnweightedAL unweighted_al_min({{v1, {v2, v3}}, {v2, {v3}}});
+
+  Graph graph_undirected_1(unweighted_al_full, false);
+  Graph graph_undirected_2(unweighted_al_min, false);
+  std::cout << "full undirected\n"
+            << graph_undirected_1.adjacency_list_str() << '\n';
+  std::cout << "min undirected\n"
+            << graph_undirected_2.adjacency_list_str() << '\n';
+
+  Graph graph_directed_1(unweighted_al_full, true);
+  Graph graph_directed_2(unweighted_al_min, true);
+  std::cout << "full directed\n"
+            << graph_directed_1.adjacency_list_str() << '\n';
+  std::cout << "min directed\n"
+            << graph_directed_2.adjacency_list_str() << '\n';
+}
+
+// The example in the comment at the top of graph.hpp.
+// All three literal representations should result in the same unweighted,
+// undirected graph.
+void example() {
+  Vertex A("A"), B("B"), C("C"), D("D"), E("E");
+
+  Graph::InputUnweightedAL rep1 = {
+      {A, {D, E}}, {B, {}}, {C, {E}}, {D, {A, E}}, {E, {A, D, C}}};
+
+  Graph::AdjacencyList rep2 = {{A, {{D, 1}, {E, 1}}},
+                               {B, {}},
+                               {C, {{E, 1}}},
+                               {D, {{A, 1}, {E, 1}}},
+                               {E, {{A, 1}, {D, 1}, {C, 1}}}};
+
+  Graph::InputUnweightedAL rep3 = {
+      {A, {D, E}}, {B, {}}, {C, {}}, {D, {E}}, {E, {C}}};
+
+  Graph graph1(rep1, false), graph2(rep2, false), graph3(rep3, false);
+
+  std::cout << "rep1\n" << graph1.adjacency_list_str() << '\n';
+  std::cout << "rep2\n" << graph2.adjacency_list_str() << '\n';
+  std::cout << "rep3\n" << graph3.adjacency_list_str() << '\n';
+}
+
+int main() {
+  // equality_op_overload();
+  // hash_overload();
+  // vertex_set_constructor();
+  // unweighted_al_constructor();
+  example();
+}
