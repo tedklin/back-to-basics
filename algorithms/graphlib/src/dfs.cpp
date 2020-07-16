b73517175da078a91825824f8b75e00e65e2d097
tedklin <tedklin@gmail.com>
2020-07-15 23:02:34 -0700

    untested dfs impl

--- algorithms/graphlib/src/dfs.cpp
+++ algorithms/graphlib/src/dfs.cpp
@@ -1,30 +1,59 @@
 #include "dfs.hpp"
 
 #include <iostream>
+#include <stack>
 
 namespace graphlib {
 
 std::map<const Vertex*, const Vertex*> dfs(
-    Graph* graph, Vertex search_root,
+    Graph* graph, const Vertex* search_root,
     void (*process_vertex_early)(const Vertex* v),
     void (*process_edge)(const Vertex* v1, const Vertex* v2, double weight),
     void (*process_vertex_late)(const Vertex* v)) {
-  // Ensure that we are referencing a Vertex within the given Graph.
-  const Vertex* root_ptr;
-  try {
-    root_ptr = graph->ptr_to_vertex(search_root);
-  } catch (std::runtime_error) {
-    std::cerr << "Warning! Tried to perform DFS with a search root that's not "
-                 "in the graph!\n\n";
-    return std::map<const Vertex*, const Vertex*>();
-  }
-
-  // Start timer
+  // Time intervals can give us valuable information about the structure of the
+  // DFS search tree (Skiena).
   int time = 0;
 
   // Map from each Vertex to its "parent" in a search tree w.r.t. the root. In a
   // DFS, this encodes "tree" edges.
   std::map<const Vertex*, const Vertex*> parent;
+
+  // Start DFS.
+  std::stack<const Vertex*> s;
+  s.push(search_root);
+
+  while (!s.empty()) {
+    const Vertex* v1 = s.top();
+    s.pop();
+
+    v1->state_ = Vertex::State::DISCOVERED;
+    v1->entry_time = time;
+    if (process_vertex_early) {
+      process_vertex_early(v1);
+    }
+
+    for (auto adj : graph->adjacent_set(*v1)) {
+      const Vertex* v2 = adj.first;
+      double weight = adj.second;
+      if (v2->state_ == Vertex::State::UNDISCOVERED) {
+        // tree edge
+        parent[v2] = v1;
+        process_edge(v1, v2, weight);
+        s.push(v2);
+      } else if ((v2->state_ != Vertex::State::PROCESSED) ||
+                 graph->is_directed()) {
+        // back edge
+        process_edge(v1, v2, weight);
+      }
+    }
+
+    if (process_vertex_late) {
+      process_vertex_late(v1);
+    }
+    ++time;
+    v1->exit_time = time;
+    v1->state_ = Vertex::State::PROCESSED;
+  }
 
   return parent;
 }
--- algorithms/graphlib/src/dfs.hpp
+++ algorithms/graphlib/src/dfs.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "bfs.hpp"
 #include "graph.hpp"
 
 #include <map>
@@ -9,13 +8,13 @@ namespace graphlib {
 
 // Traditional DFS algorithm.
 std::map<const Vertex*, const Vertex*> dfs(
-    Graph* graph, Vertex search_root,
+    Graph* graph, const Vertex* search_root,
     void (*process_vertex_early)(const Vertex* v) = nullptr,
     void (*process_edge)(const Vertex* v1, const Vertex* v2,
                          double weight) = nullptr,
     void (*process_vertex_late)(const Vertex* v) = nullptr);
 
-// Misc common vertex and edge processing functions
+// Forward declarations for common vertex and edge processing functions.
 // (common to BFS, defined in bfs.cpp)
 void print_vertex(const Vertex* v);
 void print_edge(const Vertex* v1, const Vertex* v2, double weight);
